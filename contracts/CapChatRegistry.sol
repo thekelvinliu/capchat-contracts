ed0e7c988b8e0896ef163d4cf7cbedf83cee95b4
thekelvinliu <kelvin@thekelvinliu.com>
2017-04-29 21:20:55 +0800

    refactor with some events

--- contracts/CapChatRegistry.sol
+++ contracts/CapChatRegistry.sol
@@ -1,52 +1,84 @@
 pragma solidity ^0.4.8;
 
+/// @title CapChatRegistry
+/// @author thekelvinliu <kelvin@thekelvinliu.com>
 contract CapChatRegistry {
   // variables
-  // address of current backend logic contract
-  address public currBackend;
-  // addresses of previous logic contracts
-  address[] prevBackends;
-  // maps username to user contract address
-  mapping (string => address) users;
-  // owner of this registry
+  /// owner of this registry
   address owner;
+  /// address of the curent logic contract
+  address public logicContract;
+  /// the main user registry
+  /// maps username to user contract address
+  mapping (bytes32 => address) registry;
 
   // constructor
-  // initialize this registry with the originating address
+  /// initializes a registry with the originating address
   function CapChatRegistry() {
     owner = tx.origin;
   }
 
-  // modifiers
-  modifier onlyCurrent() { if (msg.sender == currBackend) _; }
-  modifier onlyOwner() { if (msg.sender == owner) _; }
+  // events
+  event MappingAdded(bytes32 username, address caddr);
+  event MappingRemoved(bytes32 username);
+  event NewLogic(address caddr);
+  event Unauthorized(address from, string action);
 
   // functions
-  // changes the current backend logic contract to the given address
-  function changeBackend(address newBackend) onlyOwner returns (bool) {
-    // don't do anything if the given address is the same
-    if (newBackend == currBackend) return false;
-    prevBackends.push(currBackend);
-    currBackend = newBackend;
+  /// changes the current logic contract to the given address
+  /// @param newLogic the deployed address of a new logic contract
+  /// @return true if the update was successful, otherwise false
+  function updateLogic(address newLogic) returns (bool) {
+    // only allow the owner of this contract to update the logic contract
+    if (msg.sender != owner) {
+      Unauthorized(msg.sender, 'updateLogic');
+      return false;
+    }
+    // don't do anything if the given address is already the logic contract
+    if (newLogic == logicContract) return false;
+    logicContract = newLogic;
+    NewLogic(logicContract);
     return true;
   }
-  // returns true if the given user is in the mapping, otherwise false
-  function isRegistered(string user) constant returns (bool) {
-    return users[user] != address(0x0);
-  }
-  // adds the given user and contract address to the registry
-  function addUser(string user, address caddr) onlyCurrent returns (bool) {
-    // fail if the user is not already registered
-    if (!isRegistered(user)) return false;
-    users[user] = caddr;
+
+  /// adds a mapping from the given username to the given contract address
+  /// @param username the username
+  /// @param caddr the contract address
+  /// @return true if adding the mapping was successful, otherwise false
+  function add(bytes32 username, address caddr) returns (bool) {
+    // only allow the current logic contract to add mappings
+    if (msg.sender != logicContract) {
+      Unauthorized(msg.sender, 'add');
+      return false;
+    }
+    // fail if the username already has a mapping
+    if (registry[username] != address(0x0)) return false;
+    registry[username] = caddr;
+    MappingAdded(username, caddr);
     return true;
   }
-  // returns the contract address for the given user
-  function getUser(string user) onlyCurrent constant returns (address) {
-    return users[user];
+
+  /// gets the corresponding contract address for the given username
+  /// @param username the username
+  /// @return the corresponding contract address
+  function get(bytes32 username) constant returns (address) {
+    // only allow the current logic contract to get mappings
+    return (msg.sender != logicContract) ? address(0x0) : registry[username];
   }
-  // removes the given user from the registry
-  function removeUser(string user) onlyCurrent {
-    delete users[user];
+
+  /// removes the mapping for the given username
+  /// @param username the username
+  /// @return true if the removing the mapping was successful, otherwise false
+  function remove(bytes32 username) returns (bool) {
+    // only allow the current logic contract to remove mappings
+    if (msg.sender != logicContract) {
+      Unauthorized(msg.sender, 'remove');
+      return false;
+    }
+    // fail if the username does not have a mapping
+    if (registry[username] == address(0x0)) return false;
+    delete registry[username];
+    MappingRemoved(username);
+    return true;
   }
 }
